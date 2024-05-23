1d2c24c71b1bc2ebe317425874c5616d31976776
Nathan Winspear <n.winspear1@gmail.com>
2024-05-23 19:52:24 +0100

    Options page rebuild started. QuickLinks content block completed

---
+++ dependencies/theme/themeManager.js
@@ -0,0 +1,22 @@
+export const updateTheme = () => {};
+
+// const root = document.querySelector(":root"),
+//       inputs = document.querySelectorAll("input[name='theme']");
+
+// const theme = localStorage.getItem("theme-color");
+
+// const updateRoot = value => root.style.setProperty("--theme-color", `var(--${value})`);
+
+// for(const input of inputs) {
+//   if(theme && input.value === theme) {
+//     input.checked = true;
+
+//     updateRoot(theme);
+//   }
+
+//   input.onchange = e => {
+//     updateRoot(e.target.value);
+
+//     localStorage.setItem("theme-color", e.target.value);
+//   }
+// }
